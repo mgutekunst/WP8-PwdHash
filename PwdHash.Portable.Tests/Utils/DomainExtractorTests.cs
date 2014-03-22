070e74826fa8aa73281e8f02e5e98817a8aced49
Manuel Gutekunst <gutekunst@medialesson.de>
2014-03-22 10:29:47 +0100

    added a basic test

--- PwdHash.Portable.Tests/Utils/DomainExtractorTests.cs
+++ PwdHash.Portable.Tests/Utils/DomainExtractorTests.cs
@@ -1,5 +1,24 @@
-namespace PwdHash.Portable.Utils
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace PwdHash.Portable.Tests.Utils
 {
 	[TestClass]
 	public class DomainExtractorTests
+	{
+		[TestInitialize]
+		public void Init()
+			{
+	
+			}
+
+		[TestCleanup]
+		public void CleanUp()
+			{}
+
+		[TestMethod, TestCategory("Test")]
+		public void DemoTest()			
+			{
+				Assert.IsTrue(false);
+			}
+	}
 }
