39574f7cb2d69c1f583b047455ae41558bea604b
Manuel Gutekunst <gutekunst@medialesson.de>
2014-04-16 09:25:52 +0200

    recreated TestProject

--- PwdHash.Portable/Utils/DomainExtractor.cs
+++ PwdHash.Portable/Utils/DomainExtractor.cs
@@ -1,7 +1,25 @@
-﻿namespace PwdHash.Portable.Utils
+﻿using System;
+
+namespace PwdHash.Portable.Utils
 {
-    public class DomainExtractor
+    public static class DomainExtractor
     {
-         
+         public static string Extract(string url)
+		    {
+                if(!url.StartsWith("http"))
+                {
+                    url = "http://" + url;
+                }
+
+			    try
+			    {
+				    var uri = new Uri(url);
+				    return uri.Host;
+			    }
+			    catch (Exception ufe)
+			    {
+				    return String.Empty;
+			    }
+		    }
     }
 }
