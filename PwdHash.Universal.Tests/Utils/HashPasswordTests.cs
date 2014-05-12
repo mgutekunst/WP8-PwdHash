da0c44140881df5c761d1c44f71d0efc35de51cf
Manuel Gutekunst <gutekunst@medialesson.de>
2014-05-12 11:00:04 +0200

    Added HashPassword class and tests

---
+++ PwdHash.Universal.Tests/Utils/HashPasswordTests.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+using PwdHash.Utils;
+
+namespace PwdHash.Universal.Tests.Utils
+{
+    [TestClass]
+    public class HashPasswordTests
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void testToString()
+        {
+            var hashedPassword = HashPassword.create("my53cret#",
+            "example.com");
+            Assert.AreEqual("Bu6aSm+Zcsf", hashedPassword);
+        }
+
+        [TestMethod]
+        public void testToStringWithNonAsciiChars()
+        {
+            var hashedPassword = HashPassword.create("mü53crét#",
+            "example.com");
+            Assert.AreEqual("r9qeSjv+lwJ", hashedPassword);
+        }
+
+        [TestMethod]
+        public void testToStringWithNonLatin1Chars()
+        {
+            var hashedPassword = HashPassword.create("中文العربي",
+            "example.com");
+            Assert.AreEqual("AwMz3+BdMT", hashedPassword);
+        }
+
+        [TestMethod]
+        public void testToStringWithoutNonAlphanumeric()
+        {
+            var hashedPassword = HashPassword.create("my53cret",
+            "example.com");
+            Assert.AreEqual("CIUD4SCSgh", hashedPassword);
+        }
+
+        [TestMethod]
+        public void testToStringWithShortSecret()
+        {
+            var hashedPassword = HashPassword.create("ab",
+            "example.com");
+            Assert.AreEqual("0IKv", hashedPassword);
+        }
+
+        [TestMethod]
+        public void testToStringWithShortestSecret()
+        {
+            var hashedPassword = HashPassword.create("a",
+            "example.com");
+            Assert.AreEqual("9FBo", hashedPassword);
+        }
+
+        [TestMethod]
+        public void testToStringWithLongSecret()
+        {
+            var hashedPassword = HashPassword.create(
+            "abcdefghijklmnopqrstuvwxyz0123456789=", "example.com");
+            String result = hashedPassword;
+
+            // The original algorithm appends NULL bytes at the end.
+            // Those bytes should not be part of the output.
+            // "XO3u58jVa1nd+8qd08SDIQ\0\0\0\0"
+            Assert.AreEqual("XO3u58jVa1nd+8qd08SDIQ", result);
+        }
+
+        [TestMethod]
+        public void testToStringWithEmptySecret()
+        {
+            try
+            {
+                var hashedPassword = HashPassword.create("",
+                "example.com");
+                
+                Assert.Fail("App did not throw exception");
+                
+            }
+            catch (ArgumentException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+        [TestMethod]
+        public void create_emptyPassword_throwsException()
+        {
+            try
+            {
+                var hashedPassword = HashPassword.create("blabla","");
+                
+                Assert.Fail("App did not throw exception");
+                
+            }
+            catch (ArgumentException e)
+            {
+                Assert.IsTrue(true);
+            }
+        }
+
+    }
+}
---
+++ PwdHash.Universal/PwdHash.Shared/Utils/HashPassword.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Text;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Core;
+using Windows.Storage.Streams;
+
+namespace PwdHash.Utils
+{
+    public static class HashPassword
+    {
+        private static int passwordPrefix;
+        private const string _hashAlgorithm = "Md5";
+        public static string create(string password, string url)
+        {
+            if(string.IsNullOrEmpty(password) || string.IsNullOrEmpty(url))
+            {
+                throw new ArgumentException("password and url must not be empty");
+            }
+            
+            var res = hash(password, url);
+
+            var resString = CryptographicBuffer.EncodeToBase64String(res);
+            passwordPrefix = 2;
+            int length = password.Length + passwordPrefix;
+
+            return resString.Substring(0, length);
+        }
+
+        private static IBuffer hash(string key, string secret)
+        {
+            var mac = MacAlgorithmProvider.OpenAlgorithm(MacAlgorithmNames.HmacMd5);
+            var buf = CryptographicBuffer.ConvertStringToBinary(key, BinaryStringEncoding.Utf8);
+
+            var b = mac.CreateKey(buf);
+
+            CryptographicHash cHash = mac.CreateHash(CryptographicBuffer.ConvertStringToBinary(key, BinaryStringEncoding.Utf8));
+            cHash.Append(CryptographicBuffer.ConvertStringToBinary(secret, BinaryStringEncoding.Utf8));
+
+            var res = cHash.GetValueAndReset();
+            return res;
+        }
+    }
+}
