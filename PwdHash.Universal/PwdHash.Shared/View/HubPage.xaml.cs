97bbca52e117d68d27a8196ecf4418ab41bd0c14
Manuel Gutekunst <gutekunst@medialesson.de>
2014-06-27 12:53:12 +0200

    created UI. Styling is missing though

--- PwdHash.Universal/PwdHash.Shared/View/HubPage.xaml.cs
+++ PwdHash.Universal/PwdHash.Shared/View/HubPage.xaml.cs
@@ -4,9 +4,11 @@ using System.IO;
 using System.Linq;
 using System.Resources;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.ApplicationModel.Resources;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -24,12 +26,56 @@ namespace PwdHash.WinStore.View
     /// </summary>
     public sealed partial class HubPage : Page
     {
+        private PasswordBox _passwordBox;
+        private bool isMovedUp;
+
         public HubPage()
         {
             this.InitializeComponent();
             var rl = new ResourceLoader();
 
             var s = rl.GetString("ApplicationTitle");
+        }
+
+        private async void TextBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            if (e.Key == VirtualKey.Enter)
+            {
+                this.Focus(FocusState.Programmatic);
+                _passwordBox.Focus(FocusState.Programmatic);
+                MovePageUp.Begin();
+                isMovedUp = true;
+           
+            }
+#endif
+        }
+
+
+        private void PasswordBox_OnKeyDown(object sender, KeyRoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            if (e.Key == VirtualKey.Enter)
+            {
+                this.Focus(FocusState.Programmatic);
+            }
+#endif
+        }
+
+        private void PasswordBox_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _passwordBox = sender as PasswordBox;
+        }
+
+        private void PasswordBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+#if WINDOWS_PHONE_APP
+            if(isMovedUp)
+            {
+                MovePageDown.Begin();
+            }
+            isMovedUp = false;
+#endif
         }
     }
 }
--- PwdHash.Universal/PwdHash.Shared/ViewModel/MainViewModel.cs
+++ PwdHash.Universal/PwdHash.Shared/ViewModel/MainViewModel.cs
@@ -1,45 +1,99 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.ObjectModel;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using PwdHash.Utils;
+using PwdHash.WinStore.Model;
 
 namespace PwdHash.WinStore.ViewModel
 {
     public class MainViewModel : ViewModelBase
     {
+        private const int MaxRecentHashes = 20;
+
+        #region Properties
+
         private string _url;
+
         public string Url
         {
             get { return _url; }
-            set { _url = value; RaisePropertyChanged("Url"); }
+            set
+            {
+                _url = value;
+                RaisePropertyChanged("Url");
+            }
         }
 
         private string _password;
+
         public string Password
         {
             get { return _password; }
-            set { _password = value; RaisePropertyChanged("Password"); }
+            set
+            {
+                _password = value;
+                RaisePropertyChanged("Password");
+            }
         }
 
         private string _hash;
+
         public string Hash
         {
             get { return _hash; }
-            set { _hash = value; RaisePropertyChanged("Hash"); }
+            set
+            {
+                _hash = value;
+                RaisePropertyChanged("Hash");
+            }
         }
 
-        public RelayCommand HashCommand { get; set; }
+        private ObservableCollection<Hash> _recentHashes;
+
+        public ObservableCollection<Hash> RecentHashes
+        {
+            get { return _recentHashes; }
+            set
+            {
+                _recentHashes = value;
+                RaisePropertyChanged("RecentHashes");
+            }
+        }
+
+        private ObservableCollection<Hash> _favorites;
+        public ObservableCollection<Hash> Favorites
+        {
+            get { return _favorites; }
+            set { _favorites = value; RaisePropertyChanged("Favorites"); }
+        }
+
+        #endregion
+
+
+
+        #region Commands
+
+        public RelayCommand HashCommand { get; private set; }
+        public RelayCommand AddToFavoritesCommand { get; private set; }
+
+        #endregion
+
+
         public MainViewModel()
         {
-            if(IsInDesignModeStatic)
+
+            HashCommand = new RelayCommand(hash);
+            AddToFavoritesCommand = new RelayCommand(addToFavorites);
+
+            RecentHashes = new ObservableCollection<Hash>();
+            Favorites = new ObservableCollection<Hash>();
+            
+            if (IsInDesignModeStatic)
             {
                 createDesignTimeData();
             }
-
-            HashCommand = new RelayCommand(hash);
         }
+
 
         private void hash()
         {
@@ -48,13 +102,51 @@ namespace PwdHash.WinStore.ViewModel
 
             var h = HashPassword.create(Password, Url);
             Hash = h;
+
+            addToRecentHashes(new Hash{ Password = Password, Url = Url });
         }
+
+        private void addToFavorites()
+        {
+            if (string.IsNullOrEmpty(Url) || string.IsNullOrEmpty(Password))
+                return;
+
+            var hash = new Hash { Url = Url, Password = Password };
+            Favorites.Add(hash);
+        }
+
+        private void addToRecentHashes(Hash hash)
+        {
+            RecentHashes.Add(hash);
+            if (RecentHashes.Count > MaxRecentHashes)
+            {
+                RecentHashes.RemoveAt(0);
+            }
+
+            // todo : Save 
+        }
+
 
         private void createDesignTimeData()
         {
-            Url = "http://www.pwdash.com";
+            Url = "http://www.pwdhash.com";
             Password = "geheim";
             Hash = "aXnrdue8";
+
+            RecentHashes = new ObservableCollection<Hash>
+            {
+                new Hash{Url = "http://www.google.de", Password = "geheim"},
+                new Hash{Url = "http://www.google.de", Password = "geheim"},
+                new Hash{Url = "http://www.google.de", Password = "geheim"},
+            };
+
+            Favorites = new ObservableCollection<Hash>
+            {
+                new Hash{Url = "http://www.google.de", Password = "geheim"},
+                new Hash{Url = "http://www.google.de", Password = "geheim"},
+                new Hash{Url = "http://www.google.de", Password = "geheim"},
+            };
+            
         }
     }
 }
