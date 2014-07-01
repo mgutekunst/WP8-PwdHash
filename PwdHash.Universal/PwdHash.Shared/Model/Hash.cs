7039ec3e98ad25f153c37013a89c18be8dfea99e
Manuel Gutekunst <gutekunst@medialesson.de>
2014-07-01 10:22:53 +0200

    added long missing Hash.cs file

---
+++ PwdHash.Universal/PwdHash.Shared/Model/Hash.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PwdHash.WinStore.Model
+{
+    public class Hash
+    {
+        public string Url { get; set; }
+        public string Password { get; set; }
+
+        public Hash()
+        {
+            
+        }
+
+        public Hash(string url, string password)
+        {
+            Url = url;
+            Password = password;
+        }
+    }
+}
